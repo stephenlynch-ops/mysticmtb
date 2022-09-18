bfdbbebff3980d8b8dac208bae6106e23123af84
Stephen Lynch <stephen.lynch17@icloud.com>
2022-09-18 18:58:31 +0000

    FEAT: Setup post and comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((O, 'Draft'), (1, 'Published'))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    map = CloudinaryField('map', default='placeholder_map')
+    image_one = CloudinaryField('image_one', default='placeholder_one')
+    image_two = CloudinaryField('image_two', default='placeholder_two')
+    difficulty = models.CharField(max_length=10, related_name='difficulty')
+    time = models.TimeField(auto_now_add=False)
+    distance = models.CharField(max_length=10)
+    ave_speed = models.CharField(max_length=20)
+    up = models.CharField(max_length=10)
+    down = models.CharField(max_length=10)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    published_on = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+class Meta:
+    ordering = ['-created_on']
+
+
+def __str__(self):
+    return self.title
+
+
+def number_of_likes(self):
+    return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f"Comment {self.body} made by {self.name}"
+
+
