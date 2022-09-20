c9a589b5434ab5795a3d09d3a7cd0f272c7f4a42
Stephen Lynch <stephen.lynch17@icloud.com>
2022-09-20 09:52:36 +0000

    STYLE: Updated sign-out page style

--- mysticmtb/settings.py
+++ mysticmtb/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -57,6 +58,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT = '/'
 LOGOUT_REDIRECT = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',   
+}
 
 # Testing to see if this works with a bootstrap 5 pack we used 4 in the walkthrough
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
