61848c831c91f8ed7d98628c1c35bf6e644d6c53
Stephen Lynch <stephen.lynch17@icloud.com>
2022-09-21 12:09:00 +0000

    MAINT: Completed testing of models

--- blog/test_models.py
+++ blog/test_models.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Comment
 
-# Create your tests here.
+
+class TestBlogModels(TestCase):
+
+    def test_post_str(self):
+        user = User.objects.create(username="Name",)
+        post = Post.objects.create(title="Test Title", author=user)
+        self.assertEqual(post.title, "Test Title")
+
+
+    def test_comment_str_(self):
+        user = User.objects.create(username="Name",)
+        post = Post.objects.create(title="Test Title", author=user)
+        comment = Comment.objects.create(post=post, body="Some text", name="Me", approved=True)
+        self.assertEqual(comment.body, "Some text")
+        self.assertEqual(comment.name, "Me")
--- blog/test_views.py
+++ blog/test_views.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Comment
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_post_list_opens_trail_list(self):
+        response = self.client.get('/trails/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'trails.html')
+
+    def test_post_detail_operates_as_expected(self):
+        user = User.objects.create(username="Name")
+        post = Post.objects.create(title="Test Title", author=user, status=1)
+        comment = Comment.objects.create(post=post, body="Some text", name="Me", approved=True)
+        self.assertEqual(comment.body, "Some text")
+        self.assertEqual(post.status, 1)
+
+    def test_open_home_page_works(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_open_cafe_page_works(self):
+        response = self.client.get('/cafe/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cafe.html')
+    
+    def test_open_gallery_page_works(self):
+        response = self.client.get('/gallery/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'gallery.html')
+
+# the remaining functions have been manually tested as part
+# of the development and deployment steps.
