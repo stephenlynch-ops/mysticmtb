7bde7ab6c647e0e38c4c23341740c27996fd1d6a
Stephen Lynch <stephen.lynch17@icloud.com>
2022-09-21 08:29:43 +0000

    FEAT: Added message from the trails (open / countdown) element

--- blog/views.py
+++ blog/views.py
@@ -81,29 +81,31 @@ class PostLike(View):
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
-# class OpenHomePage():
-
-#     opening_time = 8
-#     closing_time = 19
-
-#     tz_London = pytz.timezone('Europe/London')
-#     datetime_London = datetime.now(tz_London)
-#     now = int(datetime_London.strftime("%H"))
-
-#     if (now >= closing_time and now < 24):
-#         hours = ((24 - now) + opening_time)
-#         context = { f"There are {hours} until the trails open tomorrow" }
-#     elif now < opening_time:
-#         hours = (opening_time - now)
-#         context = { f"There are {hours} until the trails open today" }
-#     else:
-#         context = {"The trails are open"}
-
-#     def open_home_page(self, request):
-#         return render(request, 'index.html', context)
-
 def open_home_page(request):
-    return render(request, 'index.html')
+
+    opening_time = 8
+    closing_time = 19
+
+    tz_London = pytz.timezone('Europe/London')
+    datetime_London = datetime.now(tz_London)
+    now = int(datetime_London.strftime("%H"))
+
+    if (now >= closing_time and now < 24):
+        hours = ((24 - now) + opening_time)
+        context = {
+            'message': f"There are {hours} until the trails open tomorrow"
+            }
+    elif now < opening_time:
+        hours = (opening_time - now)
+        context = {
+            'message': f"There are {hours} until the trails open today"
+            }
+    else:
+        context = {
+            'message': "The trails are OPEN"
+            }
+
+        return render(request, 'index.html', context)
 
 
 def open_cafe_page(request):
